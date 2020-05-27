13a305ef85bafc37ab603f12453c03c694b6c416
WingBot <zerongsu@163.com>
2020-05-27 18:59:42 +0800

    confirm the epsilon param value,agv will pan circle when the obstcale occurs

--- src/single_agent_static_obstacle.cpp
+++ src/single_agent_static_obstacle.cpp
@@ -30,16 +30,16 @@ int main(int argc, char **argv)
     ros::Rate loop_rate(rate);//更新频率20Hz，它会追踪记录自上一次调用Rate::sleep()后时间的流逝，并休眠直到一个频率周期的时间
  
     //初始化操作
-    double v_x_d=0.1;//向前的线速度0.2m/s
+    double v_x_d=0.2;//向前的线速度0.2m/s
     double v_y_d=0.0;//向前的线速度0.2m/s
-    double v_x_max=0.1;//向前的线速度0.2m/s
-    double v_y_max=0.1;//向前的线速度0.2m/s
+    double v_x_max=0.4;//向前的线速度0.2m/s
+    double v_y_max=0.4;//向前的线速度0.2m/s
     double v_x_t=0.0;//向前的线速度0.2m/s
     double v_y_t=0.0;//向前的线速度0.2m/s
     double v_x_k = 2.0;
     double v_y_k = 2.0;
     double obstacle_x_set = 1.0;
-    double obstacle_y_set = 0.0;
+    double obstacle_y_set = 0.1;
     
 
     double safe_distance = 0.2;//初始化障碍物位置与安全距离
@@ -97,10 +97,10 @@ int main(int argc, char **argv)
     double Bright = 0.0;
 
     
-    double epsilon = 0.05;
+    double epsilon = 0.1;//set to 0.1
     double lambda_x = 0.0;
-    double lambda_y = 0.0;
-    double Bright_k = -200.0;
+//     double lambda_y = 0.0;
+    double Bright_k = -50.0;
     double B_k = -2 ;
     
     ros::Time t_run;
@@ -135,15 +135,17 @@ int main(int argc, char **argv)
             t_run = ros::Time::now();
 //             x_d = v_x_d * (t_run - t_start).toSec();
             //计算期望位置
-            x_d = (x_start + v_x_d * (t_run - t_start).toSec()) < x_g ? (x_start +v_x_d * (t_run - t_start).toSec()):x_g;
-            y_d = y_start;
-            ROS_INFO_STREAM("x_d = "<< x_d << "\ty_d = "<< y_d);
-            ROS_INFO_STREAM("x_t = "<< x_t << "\ty_t = "<< y_t);
-            ROS_INFO_STREAM_ONCE("need to followe or not ? ");
+//             x_d = (x_start + v_x_d * (t_run - t_start).toSec()) < x_g ? (x_start +v_x_d * (t_run - t_start).toSec()):x_g;
+//             y_d = y_start;
+
+//             ROS_INFO_STREAM_ONCE("need to followe or not ? ");
             //判断与目标位置距离是否到达终点，避免航向偏移，加判断直线位移是否到达
            if(x_t < x_g && delta_goal > goal_error)
             {
-                
+                x_d = (x_start + v_x_d * (t_run - t_start).toSec()) < x_g ? (x_start +v_x_d * (t_run - t_start).toSec()):x_g;
+                y_d = y_start;
+                ROS_INFO_STREAM("x_d = "<< x_d << "\ty_d = "<< y_d);
+                ROS_INFO_STREAM("x_t = "<< x_t << "\ty_t = "<< y_t);
                 cmd_vel_pub.publish(move_cmd);
                 loop_rate.sleep();
 //                 distance = sqrt(pow((transform_.getOrigin().x() - x_start),2)+pow((transform_.getOrigin().y() - y_start),2));
@@ -159,8 +161,11 @@ int main(int argc, char **argv)
                 double delta_y = 0.0;
                 
                 //忽略太小的偏差
-                delta_x = abs(x_d - x_t) < 0.0000000000000000000001 ? 0.0 : (x_d - x_t); 
-                delta_y = abs(y_d - y_t) < 0.0000000000000000000001 ? 0.0 : (y_d - y_t); //0的范围限定
+//                 delta_x = abs(x_d - x_t) < 0.0000000000000000000001 ? 0.0 : (x_d - x_t); 
+//                 delta_y = abs(y_d - y_t) < 0.0000000000000000000001 ? 0.0 : (y_d - y_t); //0的范围限定
+                
+                delta_x = x_d - x_t; 
+                delta_y = y_d - y_t; //0的范围限定
                 ROS_INFO_STREAM("delta_x = "<< delta_x << "\tdelta_y = "<< delta_y);
 
 //                 v_x_t = abs(v_x_d + v_x_k * delta_x) > v_x_max ? v_x_max : (v_x_d + v_x_k * delta_x);
@@ -172,7 +177,7 @@ int main(int argc, char **argv)
                 ROS_INFO_STREAM("B_x = "<< B_x << "\tB_y = "<< B_y);
                 
                 double F1_x_temp = v_x_d + v_x_k * delta_x - lambda_x * B_x;
-                double F1_y_temp = v_y_d + v_y_k * delta_y - lambda_y * B_y;
+                double F1_y_temp = v_y_d + v_y_k * delta_y - lambda_x * B_y;
                 double F1_x,F1_y;
                     if (F1_x_temp > (-1)*v_x_max && F1_x_temp < v_x_max){
                             F1_x = F1_x_temp;
@@ -193,15 +198,15 @@ int main(int argc, char **argv)
 //                 ROS_INFO_STREAM();
                 
                 Bright = Bright_k * ((pow((x_t - obstacle_x),2) + pow((y_t - obstacle_y),2)) - pow(safe_distance,2));
-                double F2_x = (lambda_x + B_x * v_x_t + Bright) < 0.0000000000000000000001 ? 0.0 : (lambda_x + B_x * v_x_t + Bright);
-                double F2_y = (lambda_y + B_y * v_y_t + Bright) < 0.0000000000000000000001 ? 0.0 : (lambda_y + B_y * v_y_t + Bright);
-                ROS_INFO_STREAM("F1_x Vel= "<< F1_x << "\tF1_y Vel= "<< F1_y<<"\tF2_x = "<< F2_x << "\tF2_y = "<< F2_y);
+                double F2_x = (lambda_x + B_x * v_x_t + B_y * v_y_t + Bright) < 0.0 ? 0.0 : (lambda_x + B_x * v_x_t +B_y * v_y_t+ Bright);
+//                 double F2_y = (lambda_y   + Bright) < 0.0 ? 0.0 : (lambda_y + B_y * v_y_t + Bright);
+                ROS_INFO_STREAM("F1_x Vel= "<< F1_x << "\tF1_y Vel= "<< F1_y<<"\tF2_x = "<< F2_x);
                 
                 
                 //参数更新率
-                double lambda_rate_x = (F2_x - lambda_x) / (10 * epsilon);
-                double lambda_rate_y = (F2_y - lambda_y) / (10 * epsilon);
-                ROS_INFO_STREAM("lambda_rate_x = "<< lambda_rate_x << "\tlambda_rate_y = "<< lambda_rate_y);
+                double lambda_rate_x = (F2_x - lambda_x) / (2 * epsilon);
+//                 double lambda_rate_y = (F2_y - lambda_y) / (10 * epsilon);
+                ROS_INFO_STREAM("lambda_rate_x = "<< lambda_rate_x);
                 
                 //速度指令更新率
                 double cmd_vel_rate_x = (F1_x - v_x_t) / epsilon; 
@@ -214,10 +219,10 @@ int main(int argc, char **argv)
                 ROS_INFO_STREAM("delta_time = "<< (t_run - t_last).toSec());
                 ROS_INFO_STREAM("move_cmd.linear.x = "<< move_cmd.linear.x<< "\tmove_cmd.linear.y = "<< move_cmd.linear.y);
 //                 ROS_INFO_STREAM();
-                
+//                 lambda_x = lambda_x;
                 lambda_x = lambda_x + lambda_rate_x * (t_last -t_run).toSec();
-                lambda_y = lambda_y + lambda_rate_y * (t_last -t_run).toSec();
-                ROS_INFO_STREAM("lambda_x = "<< lambda_x<<"\tlambda_y = "<< lambda_y);
+//                 lambda_y = lambda_y + lambda_rate_y * (t_last -t_run).toSec();
+                ROS_INFO_STREAM("lambda_x = "<< lambda_x);
 //                 ROS_INFO_STREAM();
                 t_last = t_run;
                 
